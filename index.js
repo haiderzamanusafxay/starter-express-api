12ca3296db2b9cb663dafb234b3de77bfe14e32a
Haider Zaman <157501042+haidercodefreaks@users.noreply.github.com>
2024-03-08 03:34:05 +0500

    added some changes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const sendEmail = require("./sendMail.js");
 
 app.all("/sendMail", (req, res) => {
-  const { name, email, body } = req.body;
+  const { subject, to, body } = req.body;
+  sendEmail(
+    "haiderzamanyzi@gmail.com",
+    "Bluetelecast",
+    "This is a sample response from bluetelecast."
+  );
   res.status(200).json("got the respose");
 });
 app.listen(process.env.PORT || 3000);
